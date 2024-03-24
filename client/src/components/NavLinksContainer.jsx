b8290f964f7f5e6bfeabc253c5becc8a9c4c9e9f
Adib <adibahsan@iut-dhaka.edu>
2024-03-24 22:38:31 +0600

    Navlinkers removed

--- client/src/components/NavLinksContainer.jsx
+++ client/src/components/NavLinksContainer.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import NavLinkItem from './NavLinkItem';
-import BuyMeACoffeeButton from './BuyMeACoffeeButton';
-import {svgClearChat, svgDiscord, svgFaq, svgLogout} from "./util/svgs";
+import {svgClearChat, svgLogout} from "./util/svgs";
 
-const NavLinksContainer = ({ chatLog, setChatLog }) => {
+const NavLinksContainer = ({chatLog, setChatLog}) => {
     return (
-        <div className="navLinks" style={{ position: "absolute", bottom: "10px" }}>
+        <div className="navLinks" style={{position: "absolute", bottom: "10px"}}>
             {chatLog.length > 0 && (
                 <NavLinkItem
-                    svg= {svgClearChat}
+                    svg={svgClearChat}
                     text="Clear Conversations"
                     setChatLog={setChatLog}
                 />
             )}
-            {/*<NavLinkItem*/}
-            {/*    svg={svgDiscord}*/}
-            {/*    text="OpenAI Discord"*/}
-            {/*    link="https://discord.com/invite/openai"*/}
-            {/*/>*/}
-            {/*<NavLinkItem*/}
-            {/*    svg= {svgFaq}*/}
-            {/*    text="Updates & FAQ"*/}
-            {/*    link="https://help.openai.com/en/collections/3742473-chatgpt"*/}
-            {/*/>*/}
             <NavLinkItem
                 svg={svgLogout}
                 text="Log out"
